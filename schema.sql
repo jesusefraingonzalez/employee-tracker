3c0a4b0276c8d64bde4d0d0ff3c8eaf4ec9dffb6
Efrain Gonzalez <jesusgonzalez0797@gmail.com>
2021-01-19 10:27:52 -0500

    update schema file

--- schema.sql
+++ schema.sql
@@ -4,21 +4,22 @@ CREATE DATABASE employees_db;
 
 USE employees_db;
 
-CREATE TABLE department(
+CREATE TABLE departments(
     id INT NOT NULL AUTO_INCREMENT,
-    name VARCHAR(30)
+    name VARCHAR(30),
+    PRIMARY KEY(id)
 );
 
-CREATE TABLE role(
-    id INT NOT NULL,
+CREATE TABLE roles(
+    id INT NOT NULL AUTO_INCREMENT,
     title VARCHAR(30),
     salary DECIMAL,
     department_id INT NOT NULL,
     PRIMARY KEY (id)
 );
 
-CREATE TABLE employee(
-    id INT NOT NULL,
+CREATE TABLE employees(
+    id INT NOT NULL AUTO_INCREMENT,
     first_name VARCHAR(30),
     last_name VARCHAR(30),
     role_id INT NOT NULL,
