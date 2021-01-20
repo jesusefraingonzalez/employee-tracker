5a797f46fe501d7bf13b647dba811d5cc8edefbf
Efrain Gonzalez <jesusgonzalez0797@gmail.com>
2021-01-19 23:12:38 -0500

    update seed file to include more entries

--- seed.sql
+++ seed.sql
@@ -1,8 +1,14 @@
 USE employees_db;
 
-INSERT INTO departments(name) VALUES ("Accounting");
+INSERT INTO departments(name) VALUES ("Management");
 INSERT INTO departments(name) VALUES ("Sales");
+INSERT INTO departments(name) VALUES ("Accounting");
 
-INSERT INTO roles(title, salary, department_id) VALUES ("Manager", 60000, 1);
+INSERT INTO roles(title, salary, department_id) VALUES ("Regional Manager", 60000, 1);
+INSERT INTO roles(title, salary, department_id) VALUES ("Assistant to the Regional Manager", 50000, 1);
+INSERT INTO roles(title, salary, department_id) VALUES ("Salesman", 45000, 2);
+INSERT INTO roles(title, salary, department_id) VALUES ("Accountant", 45000, 2);
 
-INSERT INTO employees(first_name, last_name, role_id, manager_id) VALUES ("Dwigt", "Shrute", 1, 1);
+INSERT INTO employees(first_name, last_name, role_id, manager_id) VALUES ("Dwigt", "Schrute", 2, 1);
+INSERT INTO employees(first_name, last_name, role_id, manager_id) VALUES ("Michael", "Scott", 1, null);
+INSERT INTO employees(first_name, last_name, role_id, manager_id) VALUES ("Angela", "Martin", 4, 1);
